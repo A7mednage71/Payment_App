3759ec755b82cde486b90cf7644d5c445a70d9a6
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-05 01:19:12 +0300

    add and create app styles class

---
+++ lib/core/app_styles.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle regular18 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 18,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold18 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 18,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium9 = TextStyle(
+    color: Color(0xff808080),
+    fontSize: 9.68,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold9 = TextStyle(
+    color: Color(0xff808080),
+    fontSize: 9.68,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle regular4 = TextStyle(
+    color: Color(0xff9FA1A5),
+    fontSize: 4.09,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold24 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 24,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium12 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 12.38,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold12 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 12.38,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium25 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 25,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular5 = TextStyle(
+    color: Color(0xff3F3B2F),
+    fontSize: 5.24,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle medium22 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 22,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold20 = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle regular20 = TextStyle(
+    color: Color(0xff2F2F2F),
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+}
