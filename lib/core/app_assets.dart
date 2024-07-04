c3b993eff6703d9c1be1798de01547813e34ed63
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-05 02:00:00 +0300

    add and create app assets

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,28 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imageApplepay
+  /// assets/applepay.svg
+  static const String imageApplepay = "assets/applepay.svg";
+
+  /// Assets for imageBarcode
+  /// assets/Barcode.svg
+  static const String imageBarcode = "assets/Barcode.svg";
+
+  /// Assets for imageBasket
+  /// assets/basket.svg
+  static const String imageBasket = "assets/basket.svg";
+
+  /// Assets for imageCard
+  /// assets/card.svg
+  static const String imageCard = "assets/card.svg";
+
+  /// Assets for imageCardLogo
+  /// assets/cardLogo.svg
+  static const String imageCardLogo = "assets/cardLogo.svg";
+
+  /// Assets for imagePaypal
+  /// assets/paypal.svg
+  static const String imagePaypal = "assets/paypal.svg";
+}
+
