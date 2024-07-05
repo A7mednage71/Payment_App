4aa4fd324a6a769e8207932738837c16464df810
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-06 01:26:09 +0300

    add validation for card form

--- lib/features/checkout/presentation/views/payment_details.dart
+++ lib/features/checkout/presentation/views/payment_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/core/methods/get_app_bar.dart';
 import 'package:payment_app/features/checkout/presentation/widgets/custom_credit_card.dart';
-import 'package:payment_app/features/checkout/presentation/widgets/custom_elevated_button.dart';
 import 'package:payment_app/features/checkout/presentation/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsPage extends StatelessWidget {
@@ -11,22 +10,15 @@ class PaymentDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getMyAppBar("Payment Details"),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(height: 32),
-            const PaymentMethodslistView(),
-            const SizedBox(height: 34),
-            const CustomCreditCard(),
-            const SizedBox(height: 34),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: CustomElevatedButton(
-                text: "Pay",
-                onPressed: () {},
-              ),
-            )
+            SizedBox(height: 32),
+            PaymentMethodslistView(),
+            SizedBox(height: 34),
+            CustomCreditCard(),
+            SizedBox(height: 34),
           ],
         ),
       ),
--- lib/features/checkout/presentation/widgets/custom_credit_card.dart
+++ lib/features/checkout/presentation/widgets/custom_credit_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:payment_app/features/checkout/presentation/widgets/custom_elevated_button.dart';
 
 class CustomCreditCard extends StatefulWidget {
   const CustomCreditCard({super.key});
@@ -16,10 +17,12 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
   bool showBackView = false;
 
   GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         CreditCardWidget(
           cardNumber: cardNumber,
@@ -31,6 +34,7 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
           isHolderNameVisible: true,
         ),
         CreditCardForm(
+          autovalidateMode: autovalidateMode,
           cardNumber: cardNumber,
           expiryDate: expiryDate,
           cardHolderName: cardHolderName,
@@ -44,6 +48,22 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
             setState(() {});
           },
           formKey: formKey,
+        ),
+        const SizedBox(height: 35),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: CustomElevatedButton(
+            text: "Pay",
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
         )
       ],
     );
