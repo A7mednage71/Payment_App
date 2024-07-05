b68f8cbb35a616aa49f4083ccbcb83bbd4ea9e0e
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-05 20:56:20 +0300

    add CustomElevatedButton to payment details screen

--- lib/features/checkout/presentation/views/payment_details.dart
+++ lib/features/checkout/presentation/views/payment_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/core/methods/get_app_bar.dart';
 import 'package:payment_app/features/checkout/presentation/widgets/custom_credit_card.dart';
+import 'package:payment_app/features/checkout/presentation/widgets/custom_elevated_button.dart';
 import 'package:payment_app/features/checkout/presentation/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsPage extends StatelessWidget {
@@ -10,13 +11,22 @@ class PaymentDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getMyAppBar("Payment Details"),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 32),
-            PaymentMethodslistView(),
-            SizedBox(height: 34),
-            CustomCreditCard(),
+            const SizedBox(height: 32),
+            const PaymentMethodslistView(),
+            const SizedBox(height: 34),
+            const CustomCreditCard(),
+            const SizedBox(height: 34),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: CustomElevatedButton(
+                text: "Pay",
+                onPressed: () {},
+              ),
+            )
           ],
         ),
       ),
