9115738ea3b283206e2b2a54ae5a4b1e5a76e2c9
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-08 22:08:03 +0300

    create StripeService with initiPaymentIntent

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,7 +1,18 @@
+import 'package:payment_app/core/utils/api_keys.dart';
+import 'package:payment_app/core/utils/api_service.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
-  // PaymentIntentModel createPaymentIntent(
-  //     PaymentIntentInputModel intentinputmodel) {}
+  final apiService = ApiService();
+  String baseUrl = "https://api.stripe.com/v1/payment_intents";
+  Future<PaymentIntentModel> initiPaymentIntent(
+      PaymentIntentInputModel paymentintentinputmodel) async {
+    var response = await apiService.post(
+        body: paymentintentinputmodel.tojson(),
+        url: baseUrl,
+        token: ApiKeys.stripeToken);
+    var paymentmodel = PaymentIntentModel.fromJson(response.data);
+    return paymentmodel;
+  }
 }
