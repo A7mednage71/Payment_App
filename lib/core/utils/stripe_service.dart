81e103f9c8fbc79bceff7058f60153f8041051a1
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-07-08 20:51:46 +0300

    add  dio and create api service with post method

---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,7 @@
+import 'package:payment_app/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  // PaymentIntentModel createPaymentIntent(
+  //     PaymentIntentInputModel intentinputmodel) {}
+}
