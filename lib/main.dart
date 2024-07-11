3ccf45c96a8abdfb8377c4460742ac7aa5187378
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-12 01:51:48 +0300

    add stripe publish key

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:payment_app/core/utils/api_keys.dart';
 import 'package:payment_app/features/checkout/presentation/views/MyCartScreen.dart';
 
 void main() {
+  Stripe.publishableKey = ApiKeys.publicKeyToken;
   runApp(const PaymentApp());
 }
 
