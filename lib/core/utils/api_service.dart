e0b09e67d016d3161c0b9d86fbfdc725f42037c0
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-12 04:50:27 +0300

    add all stripe methods descriptions to api service as comments

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -19,3 +19,27 @@ class ApiService {
     return response;
   }
 }
+
+
+// # Create a Customer (use an existing Customer ID if this is a returning customer)
+// curl https://api.stripe.com/v1/customers \
+//   -u sk_test_51PZypMRtGBHRxRYXJSNIcCFZUzT775fcV2BqH1A2vaIq021ZLp5uK0q8aZFO6nNtzIGZjuFCC2G19XryAIwyuhYw005YnKQ5Q9: \
+//   -X "POST"
+
+// # Create an Ephemeral Key for the Customer
+// curl https://api.stripe.com/v1/ephemeral_keys \
+//   -u sk_test_51PZypMRtGBHRxRYXJSNIcCFZUzT775fcV2BqH1A2vaIq021ZLp5uK0q8aZFO6nNtzIGZjuFCC2G19XryAIwyuhYw005YnKQ5Q9: \
+//   -H "Stripe-Version: 2024-06-20" \
+//   -X "POST" \
+//   -d "customer"="{{CUSTOMER_ID}}" \
+
+// # Create a PaymentIntent
+// curl https://api.stripe.com/v1/payment_intents \
+//   -u sk_test_51PZypMRtGBHRxRYXJSNIcCFZUzT775fcV2BqH1A2vaIq021ZLp5uK0q8aZFO6nNtzIGZjuFCC2G19XryAIwyuhYw005YnKQ5Q9: \
+//   -X "POST" \
+//   -d "customer"="{{CUSTOMER_ID}}" \
+//   -d "amount"=1099 \
+//   -d "currency"="eur" \
+//   # In the latest version of the API, specifying the `automatic_payment_methods` parameter
+//   # is optional because Stripe enables its functionality by default.
+//   -d "automatic_payment_methods[enabled]"=true \
